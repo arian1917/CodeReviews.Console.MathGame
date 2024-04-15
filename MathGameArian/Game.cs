8ebf82c697505e9f11d64c755e11d754656380f7
arian1917 <37401897+arian1917@users.noreply.github.com>
2024-04-15 10:59:13 +0200

    Math Game Arian Calvo

---
+++ MathGameArian/Game.cs
@@ -0,0 +1,18 @@
+namespace Academy.Console.MathGame.Models; 
+
+internal class Game{
+    public DateTime Date { get; set; }
+
+    public int Score {get; set; }
+
+    public GameType Type {get; set; }
+}
+
+internal enum GameType{
+    Addition, 
+    Substraction, 
+    Multiplication, 
+    Division, 
+    Random
+
+}
